8e58eb7839f9b284b6586a9b540f6f0b161d41d6
Esteban-Montecinos <estebanmontecinos2001@gmail.com>
2023-09-15 18:20:18 -0300

    constants

---
+++ src/app/constants.js
@@ -0,0 +1,19 @@
+export const components = [
+    {name: "Alert", href: "/docs/components/alert"},
+    {name: "Avatar", href: "/docs/components/avatar"},
+    {name: "Badge", href: "/docs/components/badge"},
+    {name: "Button", href: "/docs/components/button"},
+    {name: "Card", href: "/docs/components/card"},
+    {name: "Divider", href: "/docs/components/divider"},
+    {name: "Form", href: "/docs/components/form"},
+    {name: "Input", href: "/docs/components/input"},
+    {name: "Label", href: "/docs/components/label"},
+    {name: "Skeleton", href: "/docs/components/skeleton"},
+    {name: "Spinner", href: "/docs/components/spinner"},
+    {name: "Table", href: "/docs/components/table"},
+    {name: "Textarea", href: "/docs/components/textarea"},
+    {name: "User", href: "/docs/components/user"},
+]
+export const started = [
+    {name: "Introduction", href: "/docs"}
+]
