39b46ff1581c630e9fdae87e593ec291ccd3def4
Esteban-Montecinos <estebanmontecinos2001@gmail.com>
2023-09-15 18:31:46 -0300

    code preview

--- src/app/docs/components/avatar/bordered-avatars.jsx
+++ src/app/docs/components/avatar/bordered-avatars.jsx
@@ -1,29 +1,25 @@
 export default function BorderedAvatars() {
-    const code = `<img 
-    className="w-12 h-12 p-[.15rem] rounded-full ring-2 ring-emerald-500" 
-    src="https://github.com/Esteban-Montecinos.png" 
-    alt="avatar of Esteban-Montecinos" 
-/>`;
   return (
     <div className="flex flex-col gap-2">
-        <h2 className="text-xl font-semibold">Bordered</h2>
-        <div className="flex flex-row justify-center gap-4 p-6 border rounded-t-lg border-neutral-700">
-          <img
-            className="w-12 h-12 p-[.15rem] rounded-full ring-2 ring-emerald-500"
-            src="https://github.com/Esteban-Montecinos.png"
-            alt="avatar of Esteban-Montecinos"
-          />
-          <img
-            className="w-12 h-12 p-[.15rem] rounded ring-2 ring-emerald-500"
-            src="https://github.com/Esteban-Montecinos.png"
-            alt="Rounded avatar"
-          />
-        </div>
-        <div className="flex flex-col gap-2 p-6 border rounded-b-lg border-neutral-700">
-          <pre>
-            <code>{code}</code>
-          </pre>
-        </div>
+      <h2 className="text-xl font-semibold">Bordered</h2>
+      <div className="flex flex-row justify-center gap-4 p-6 border rounded-t-lg border-neutral-700">
+        <img
+          className="w-12 h-12 p-[.15rem] rounded-full ring-2 ring-emerald-500"
+          src="https://github.com/Esteban-Montecinos.png"
+          alt="avatar of Esteban-Montecinos"
+        />
+        <img
+          className="w-12 h-12 p-[.15rem] rounded ring-2 ring-emerald-500"
+          src="https://github.com/Esteban-Montecinos.png"
+          alt="Rounded avatar"
+        />
       </div>
-  )
+      <div className="flex flex-col gap-2 p-6 text-sm border rounded-b-lg border-neutral-700">
+        &lt;img className=&quot;w-12 h-12 p-[.15rem] rounded-full ring-2 ring-emerald-500&quot;<br/>
+        &nbsp;&nbsp;src=&quot;https://github.com/Esteban-Montecinos.png&quot;<br/>
+        &nbsp;&nbsp;alt=&quot;avatar of Esteban-Montecinos&quot;<br/>
+        /&gt;
+      </div>
+    </div>
+  );
 }
--- src/app/docs/components/avatar/image-avatars.jsx
+++ src/app/docs/components/avatar/image-avatars.jsx
@@ -1,30 +1,29 @@
 export default function ImageAvatars() {
-    const code = `<img 
-    className="w-12 h-12 rounded-full" 
-    src="https://github.com/Esteban-Montecinos.png" 
-    alt="avatar of Esteban-Montecinos" 
-/>`;
   return (
     <div className="flex flex-col gap-2">
-        <h2 className="text-xl font-semibold">Image avatars</h2>
-        <p className="text-sm text-neutral-400">Avatar created with image</p>
-        <div className="flex flex-row justify-center gap-4 p-6 border rounded-t-lg border-neutral-700">
-          <img
-            className="w-12 h-12 rounded-full"
-            src="https://github.com/Esteban-Montecinos.png"
-            alt="avatar of Esteban-Montecinos"
-          />
-          <img
-            className="w-12 h-12 rounded"
-            src="https://github.com/Esteban-Montecinos.png"
-            alt="Rounded avatar"
-          />
-        </div>
-        <div className="flex flex-col gap-2 p-6 border rounded-b-lg border-neutral-700">
-          <pre>
-            <code>{code}</code>
-          </pre>
-        </div>
+      <h2 className="text-xl font-semibold">Image avatars</h2>
+      <p className="text-sm text-neutral-400">Avatar created with image</p>
+      <div className="flex flex-row justify-center gap-4 p-6 border rounded-t-lg border-neutral-700">
+        <img
+          className="w-12 h-12 rounded-full"
+          src="https://github.com/Esteban-Montecinos.png"
+          alt="avatar of Esteban-Montecinos"
+        />
+        <img
+          className="w-12 h-12 rounded"
+          src="https://github.com/Esteban-Montecinos.png"
+          alt="Rounded avatar"
+        />
       </div>
-  )
+      <div className="flex flex-col gap-2 p-6 text-sm border rounded-b-lg border-neutral-700">
+        &lt;img className=&quot;w-12 h-12 rounded-full&quot;
+        <br />
+        &nbsp;&nbsp;src=&quot;https://github.com/Esteban-Montecinos.png&quot;
+        <br />
+        &nbsp;&nbsp;alt=&quot;avatar of Esteban-Montecinos&quot;
+        <br />
+        /&gt;
+      </div>
+    </div>
+  );
 }
