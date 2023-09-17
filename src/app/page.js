93574e50049a66f1c6dee1aefed8bd13d4c6846d
Esteban-Montecinos <estebanmontecinos2001@gmail.com>
2023-09-16 23:39:09 -0300

    style home

--- src/app/page.js
+++ src/app/page.js
@@ -3,9 +3,9 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex flex-col gap-4 p-2 m-auto max-w-7xl">
-      <h1 className="text-3xl font-bold leading-tight tracking-tighter md:text-5xl">Build your component library.</h1>
-      <span className="text-lg text-neutral-400">Beautifully designed components that you can copy and paste into your apps. Accessible. Customizable. Open Source.</span>
+    <main className="flex flex-col min-h-screen gap-6 p-2 m-auto max-w-7xl">
+      <h1 className="text-3xl font-bold leading-tight tracking-tighter mt-14 md:text-5xl">Tips and tricks about Next.js components.</h1>
+      <span className="text-lg text-neutral-400">Next.js components that you can copy and paste into your apps, customizable and open source.</span>
       <Link target="_blank" rel="noreferrer" className="inline-flex items-center justify-center gap-2 px-4 py-2 text-sm transition-colors duration-200 ease-in-out border rounded-md w-28 h-9 border-neutral-700 hover:bg-neutral-700" href="https://github.com/Esteban-Montecinos/mntui"><IconBrandGithub/> GitHub</Link>
       <div className="flex items-center w-full gap-4 px-4 py-2 text-yellow-100 rounded-md bg-yellow-950">
           <IconAlertTriangle />
