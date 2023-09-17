34c92e66c8a02e52928f0e41fb54b8230463db7f
Esteban-Montecinos <estebanmontecinos2001@gmail.com>
2023-09-16 22:26:35 -0300

    resolve path

--- src/app/ui/read-file.js
+++ src/app/ui/read-file.js
@@ -2,7 +2,7 @@ import { readFileSync } from "fs";
 import path from "path";
 
 export const GetFileBySlug = ({ filename }) => {
-  const file = path.join(process.cwd(), filename);
+  const file = path.resolve(process.cwd(), filename);
   const data = readFileSync(file, "utf-8");
   return data;
 };
